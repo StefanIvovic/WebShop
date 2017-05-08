321c9fc6996794c37749ce567702349fbf540566
Stefan Ivovic <stefanivovic91@gmail.com>
2017-05-08 13:07:22 +0200

    test desc

---
+++ WebShop.Tests/Controllers/TestProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebShop.BusinessEntities;
+using WebShop.BusinessServices;
+using WebShop.Controllers;
+
+namespace WebShop.Tests
+{
+    [TestClass]
+    public class TestProductController
+    {
+        // pokusavam da nadjem dobar tutorial 
+        // gde je objasnjeno detaljije mockovanje servisa
+        // i kako koristiti mock objekat u testiranju     
+    }
+}
